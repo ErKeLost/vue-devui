bc5ef0156239419acb909d9bb61ad2e14140babf
erkelost <1256029807@qq.com>
2022-12-07 11:31:22 +0800

    test: popover

--- packages/devui-vue/devui/popover-refactor/src/popover-refactor.tsx
+++ packages/devui-vue/devui/popover-refactor/src/popover-refactor.tsx
@@ -1,8 +1,9 @@
-import { defineComponent, toRefs } from 'vue';
+import { defineComponent, onMounted, toRefs } from 'vue';
 import type { SetupContext } from 'vue';
 import { popoverRefactorProps, PopoverRefactorProps } from './popover-refactor-types';
 import './popover-refactor.scss';
 import { computePosition, flip } from '@floating-ui/dom';
+import { throttle } from 'lodash';
 export default defineComponent({
   name: 'DPopoverRefactor',
   props: popoverRefactorProps,
@@ -11,13 +12,32 @@ export default defineComponent({
     // 直接解构 props 会导致响应式失效，需要使用 toRefs 进行包裹
     // const { data } = toRefs(props);
     // console.log(data.value);
-    computePosition(referenceEl, floatingEl, {
-      middleware: [flip()],
+    onMounted(() => {
+      const reference = document.getElementById('reference');
+      const floating = document.getElementById('floating');
+      function handleWindowScroll() {
+        computePosition(reference, floating, {
+          placement: 'top',
+          // Try removing this line below. The tooltip will
+          // overflow the viewport's edge!
+          middleware: [flip()],
+        }).then(({ x, y }) => {
+          Object.assign(floating.style, {
+            top: `${y}px`,
+            left: `${x}px`,
+          });
+        });
+      }
+      const scroll = throttle(handleWindowScroll, 200);
+      window.addEventListener('scroll', scroll);
     });
     return () => {
-      return <div class="devui-popover-refactor">
-
-      </div>;
+      return (
+        <div class="devui-popover-refactor">
+          <div id="reference"></div>
+          <div id="floating">Tooltip</div>
+        </div>
+      );
     };
   },
 });
